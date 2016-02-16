47a6b3f43b5cc9c5d24428f31bdf14ef30acc2a2
Timothy Chilvers <chilvman@gmail.com>
2016-02-16 17:28:36 +0000

    Add torrent search

---
+++ routes/search.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var tpb = require('thepiratebay');
+
+router.get('/', function(req, res, next) {
+  console.log('searching ' + tpb);
+  tpb.search(req.query.q, {}, function (err, searchResult) {
+    console.log(searchResult);
+    res.render('search', { results: searchResult, query : req.query.q });
+  })
+
+
+});
+
+module.exports = router;
